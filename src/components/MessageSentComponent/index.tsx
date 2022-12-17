b24d1a6b1151937072d58a49c84602bac758917a
rikvik2006 <75540124+rikvik2006@users.noreply.github.com>
2022-12-17 16:22:17 +0100

    Styled message component

--- src/components/MessageSentComponent/index.tsx
+++ src/components/MessageSentComponent/index.tsx
@@ -1,5 +1,8 @@
 import { FC } from 'react'
-import { Message } from '../../utils/interefaces'
+import { Message } from '../../utils/interefaces';
+import style from "./index.module.scss";
+import moment from "moment";
+
 
 type Props = {
     message: Message
@@ -8,7 +11,20 @@ type Props = {
 
 const MessageSentComponent: FC<Props> = ({ message }) => {
     return (
-        <div>{message.content}</div>
+        <div className={style.messageContainer}>
+            <div className={style.authorAvatar}>
+                <img src={message.author.avatar} alt={`${message.author.username} avater's`} />
+            </div>
+            <div className={style.message}>
+                <div className={style.messageInformation}>
+                    <div className={style.messageAuthor}>
+                        <div className={style.name}>{message.author.username}</div>
+                    </div>
+                    <div className={style.messageTime}>{moment(message.time).format("DD/MM/YYYY HH:mm")}</div>
+                </div>
+                <div className={style.messageContent}>{message.content}</div>
+            </div>
+        </div>
     )
 }
 
