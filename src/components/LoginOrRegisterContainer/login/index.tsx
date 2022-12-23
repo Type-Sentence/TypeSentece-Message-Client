1ad33e7520ffe90ad2317b87de7c906b767e3401
rikvik2006 <75540124+rikvik2006@users.noreply.github.com>
2022-12-23 23:52:08 +0100

    Created the login and the register container

---
+++ src/components/LoginOrRegisterContainer/login/index.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react'
+
+type Props = {}
+
+
+const LoginContainer: FC<Props> = () => {
+    return (
+        <div>LoginContainer</div>
+    )
+}
+
+export default LoginContainer
---
+++ src/components/LoginOrRegisterContainer/register/index.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react'
+
+type Props = {}
+
+
+const RegisterContainer: FC<Props> = () => {
+    return (
+        <div>RegisterContainer</div>
+    )
+}
+
+export default RegisterContainer
