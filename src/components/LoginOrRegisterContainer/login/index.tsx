0414073f719eee4602cb19ac8d859911767d4686
rikvik2006 <75540124+rikvik2006@users.noreply.github.com>
2022-12-27 03:04:16 +0100

    Creating the login form

--- src/components/LoginOrRegisterContainer/login/index.tsx
+++ src/components/LoginOrRegisterContainer/login/index.tsx
@@ -7,7 +7,17 @@ type Props = {}
 const LoginContainer: FC<Props> = () => {
     return (
         <div className={style.container}>
-            Login
+            <div className={style.titleContainer}>
+                <span className={style.title}>Login</span>
+                <span className={style.switcher}> or Register</span>
+            </div>
+
+            <form className={style.formContainer}>
+                <input type="text" className={style.inputField} placeholder="email" />
+                <input type="text" className={style.inputField} placeholder="password" />
+                <div className={style.forgotPw}>Forgot password</div>
+                <input type="submit" value="Sing Up" />
+            </form>
         </div>
     )
 }
