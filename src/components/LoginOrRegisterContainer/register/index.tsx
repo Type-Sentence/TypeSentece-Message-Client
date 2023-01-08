c7b66f07ac652b11c454518c35fc4d1d0c77e5ff
rikvik2006 <75540124+rikvik2006@users.noreply.github.com>
2023-01-08 01:35:27 +0100

    Created the register form

--- src/components/LoginOrRegisterContainer/register/index.tsx
+++ src/components/LoginOrRegisterContainer/register/index.tsx
@@ -1,11 +1,28 @@
 import { FC } from 'react'
+import style from "../fields.module.scss";
 
-type Props = {}
+type Props = {
+    changeRegisterMode(): void;
+}
 
 
-const RegisterContainer: FC<Props> = () => {
+const RegisterContainer: FC<Props> = ({ changeRegisterMode }) => {
     return (
-        <div>RegisterContainer</div>
+        <div className={style.container}>
+            <div className={style.titleContainer}>
+                <span className={style.title}>Register</span>
+                <span className={style.switcher} onClick={changeRegisterMode}> or Login</span>
+            </div>
+
+            <form className={style.formContainer}>
+                <input type="text" className={style.inputField} placeholder="username" />
+                <input type="text" className={style.inputField} placeholder="email" />
+                <input type="password" className={style.inputField} placeholder="password" />
+                <input type="password" className={style.inputField} placeholder="verify password" />
+
+                <input type="submit" value="Sing Up" />
+            </form>
+        </div>
     )
 }
 
