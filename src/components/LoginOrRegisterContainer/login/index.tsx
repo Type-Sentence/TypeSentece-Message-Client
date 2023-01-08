abcf431bbeec62636187e2e0005d752ec3209a20
rikvik2006 <75540124+rikvik2006@users.noreply.github.com>
2023-01-08 01:26:04 +0100

    Styled the login form

--- src/components/LoginOrRegisterContainer/login/index.tsx
+++ src/components/LoginOrRegisterContainer/login/index.tsx
@@ -1,21 +1,30 @@
 import { FC } from 'react'
 import style from "../fields.module.scss";
+import Link from 'next/link';
 
-type Props = {}
+type Props = {
+    changeRegisterMode(): void;
+}
 
 
-const LoginContainer: FC<Props> = () => {
+const LoginContainer: FC<Props> = ({ changeRegisterMode }) => {
     return (
         <div className={style.container}>
             <div className={style.titleContainer}>
                 <span className={style.title}>Login</span>
-                <span className={style.switcher}> or Register</span>
+                <span className={style.switcher} onClick={changeRegisterMode}> or Register</span>
             </div>
 
             <form className={style.formContainer}>
                 <input type="text" className={style.inputField} placeholder="email" />
                 <input type="text" className={style.inputField} placeholder="password" />
-                <div className={style.forgotPw}>Forgot password</div>
+                <div className={style.optionsContainer}>
+                    <div className={style.rememberMeContainer}>
+                        <input type="checkbox" id="remember-me" />
+                        <label htmlFor="remember-me">Remember me</label>
+                    </div>
+                    <div className={style.forgotPw}>Forgot password?</div>
+                </div>
                 <input type="submit" value="Sing Up" />
             </form>
         </div>
